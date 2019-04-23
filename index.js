3a9455a513088bb63f360b6fea157be3e63fcfb2
github username <itsDario1@gmail.com>
2019-04-23 11:15:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const input = document.getElementById('main');
+    input.addEventListener('click', function (event) {
+        alert('I was clicked!');
+    });
 }
